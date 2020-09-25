cfc4f9215e44863a991133befec022d946261c11
lpdft <772346730@qq.com>
2020-09-25 17:47:22 +0800

    [ruyue] feat:adjust single goods component style

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
-import '../style/Home.css'
+import '../style/Home.css';
+import apple from '../apple.jpg';
 
 class Home extends Component {
+  state = {
+    name: "苹果",
+    price: 2,
+    unitOfMeasurement: "斤",
+    img: apple,
+  }
   render() {
-    const { name, price, unit, img, onAdd } = this.props;
+    // const { name, price, unit, img, onAdd } = this.props;
     return (
       <div className="goods">
-        <img src={img} alt="goods_img"/>
+        <img src={this.state.img} alt="goods_img"/>
         <div className="buy">
-          <span className="name">{name}</span>
-          <span className="price">单价：{price}元/{unit}</span>
-          <button onClick={onAdd}>+</button>
+          <span className="name">{this.state.name}</span>
+          <span className="price">单价：{this.state.price}元/{this.state.unitOfMeasurement}</span>
         </div>
+        <button>+</button>
       </div>
     );
   }
