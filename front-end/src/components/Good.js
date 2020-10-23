33827cc6e28f375965a52acf731f357569449b58
lpdft <772346730@qq.com>
2020-10-23 09:02:34 +0800

    [ruyue] fix:adjust good display style

--- front-end/src/components/Good.js
+++ front-end/src/components/Good.js
@@ -12,14 +12,26 @@ class Good extends Component {
   render() {
     // const { name, price, unit, img, onAdd } = this.props;
     return (
-      <div className="goods">
-        <img src={this.state.img} alt="goods_img"/>
-        <div className="buy">
-          <span className="name">{this.state.name}</span>
-          <span className="price">单价：{this.state.price}元/{this.state.unitOfMeasurement}</span>
+      <div class="row row-cols-1 row-cols-md-3">
+        <div class="col mb-4">
+          <div class="card">
+            <img src={this.state.img} class="card-img-top" alt="goods_img"/>
+            <div class="card-body">
+              <h5 class="card-title">{this.state.name}</h5>
+              <p class="card-text">单价：{this.state.price}元/{this.state.unitOfMeasurement}</p>
+            </div>
+            <button>十</button>
+          </div>
         </div>
-        <button>+</button>
       </div>
+      // <div className="goods">
+      //   <img >
+      //   <div className="buy">
+      //     <span className="name">{this.state.name}</span>
+      //     <span className="price">单价：{this.state.price}元/{this.state.unitOfMeasurement}</span>
+      //   </div>
+      //   <button>+</button>
+      // </div>
     );
   }
 }
